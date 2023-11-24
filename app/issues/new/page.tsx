d10a3cad7580cd9011ea7059ad217d0ca1bac2e7
NicolasLi <linze201@gmail.com>
2023-11-24 22:02:47 +0800

    build the new issue page

---
+++ app/issues/new/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+import { TextField, TextArea, Button } from '@radix-ui/themes';
+export default function NewIssuePage() {
+  return (
+    <div className='max-w-xl space-y-3'>
+      <TextField.Root>
+        <TextField.Input placeholder='Title' />
+      </TextField.Root>
+      <TextArea placeholder='Description' />
+      <Button>Submit New Issue</Button>
+    </div>
+  );
+}
