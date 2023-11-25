c97fea8804dae7e1f351b7a2f6fe225021633637
NicolasLi <linze201@gmail.com>
2023-11-25 19:06:03 +0800

    refactor: organize import statement

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,9 +1,8 @@
 import prisma from '@/prisma/client';
 import { Table } from '@radix-ui/themes';
-import IssueStatusBadge from '../components/IssueStatusBadge';
+import { IssueStatusBadge, Link } from '@/app/components';
 import delay from 'delay';
 import IssueActions from './IssueActions';
-import Link from '../components/Link';
 
 export default async function IssuesPage() {
   const issues = await prisma.issue.findMany();
