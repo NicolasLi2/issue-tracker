51762ff948b7e334e64a56cba8f39445807b2a36
NicolasLi <linze201@gmail.com>
2023-11-27 16:00:28 +0800

    add login and logout links

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -3,9 +3,12 @@ import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { AiFillBug } from 'react-icons/ai';
 import classnames from 'classnames';
+import { useSession } from 'next-auth/react';
+import { Box } from '@radix-ui/themes';
 
 export default function NavBar() {
   const currentPath = usePathname(); // This hook is dependent on browser APIs, so need to convert this component to client component
+  const { status, data: session } = useSession();
 
   const links = [
     { label: 'Dashboard', href: '/' },
@@ -19,19 +22,28 @@ export default function NavBar() {
       </Link>
       <ul className='flex space-x-6'>
         {links.map((link) => (
-          <Link
-            key={link.href}
-            className={classnames({
-              'text-zinc-900': link.href === currentPath,
-              'text-zinc-500': link.href !== currentPath,
-              'hover:text-zinc-800 transition-colors': true,
-            })}
-            href={link.href}
-          >
-            {link.label}
-          </Link>
+          <li key={link.href}>
+            <Link
+              className={classnames({
+                'text-zinc-900': link.href === currentPath,
+                'text-zinc-500': link.href !== currentPath,
+                'hover:text-zinc-800 transition-colors': true,
+              })}
+              href={link.href}
+            >
+              {link.label}
+            </Link>
+          </li>
         ))}
       </ul>
+      <Box>
+        {status === 'authenticated' && (
+          <Link href='/api/auth/signout'>Log out</Link>
+        )}
+        {status === 'unauthenticated' && (
+          <Link href='/api/auth/signin'>Login</Link>
+        )}
+      </Box>
     </nav>
   );
 }
