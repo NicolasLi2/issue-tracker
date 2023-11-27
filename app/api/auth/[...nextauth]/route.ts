653ae67ab44136a911b817290e5830eab11e50fa
NicolasLi <linze201@gmail.com>
2023-11-27 15:33:56 +0800

    add prisma adapter

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,25 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import prisma from '@/prisma/client';
 
 const handler = NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      httpOptions: {
+        timeout: 40000,
+      },
     }),
   ],
+  // when use adapter, next-auth change session strategy to JWT to database,
+  // but database strategy does not work with OS providers like Google,
+  // so need to set session strategy to JWT
+  session: {
+    strategy: 'jwt',
+  },
 });
 
 export { handler as GET, handler as POST };
