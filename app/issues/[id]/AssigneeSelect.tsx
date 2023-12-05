692769894547d0e5517ee93a173899f22b039d67
nicolas <linze201@gmail.com>
2023-12-05 17:30:45 +0800

    Refactor AssigneeSelect component

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -3,39 +3,31 @@ import { Issue, User } from '@prisma/client';
 import { Select } from '@radix-ui/themes';
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
-import { useEffect, useState } from 'react';
-import Skeleton from 'react-loading-skeleton';
 import toast, { Toaster } from 'react-hot-toast';
+import Skeleton from 'react-loading-skeleton';
 
 export default function AssigneeSelect({ issue }: { issue: Issue }) {
-  const {
-    data: users,
-    error,
-    isLoading,
-  } = useQuery<User[]>({
-    queryKey: ['users'],
-    queryFn: () => axios.get('/api/users').then((res) => res.data),
-    staleTime: 60 * 1000, // 60s
-    retry: 3,
-  });
+  const { data: users, error, isLoading } = useUsers();
 
   if (isLoading) return <Skeleton />;
 
   if (error) return null;
 
+  const assignIssue = (userId: string) => {
+    axios
+      .patch('/api/issues/' + issue.id, {
+        assignedToUserId: userId || null,
+      })
+      .catch(() => {
+        toast.error('Changes could not be saved');
+      });
+  };
+
   return (
     <>
       <Select.Root
         defaultValue={issue.assignedToUserId || ''}
-        onValueChange={(userId) => {
-          axios
-            .patch('/api/issues/' + issue.id, {
-              assignedToUserId: userId || null,
-            })
-            .catch(() => {
-              toast.error('Changes could not be saved');
-            });
-        }}
+        onValueChange={assignIssue}
       >
         <Select.Trigger placeholder='Assign...' />
         <Select.Content>
@@ -54,3 +46,11 @@ export default function AssigneeSelect({ issue }: { issue: Issue }) {
     </>
   );
 }
+
+const useUsers = () =>
+  useQuery<User[]>({
+    queryKey: ['users'],
+    queryFn: () => axios.get('/api/users').then((res) => res.data),
+    staleTime: 60 * 1000, // 60s
+    retry: 3,
+  });
